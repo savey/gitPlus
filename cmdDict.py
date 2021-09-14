0fd2bebbc3779bfef4eef6524a9d31a35dcdc006
Savey <64387552@qq.com>
2021-09-14 20:45:20 +0800

    fix

--- cmdDict.py
+++ cmdDict.py
@@ -2,11 +2,11 @@ from support import gitCmdStrategy,printCurrentBrStrategy,gtaskHelp,createTaskSt
 
 
 """
-	这里配置下指令对应的命令解释器
+	这里配置下指令对应的命令解释器/目前只支持短标签 …………
 """
 cmd = {
 	"git": gitCmdStrategy.GitCmd(),
-	"-b|branch": printCurrentBrStrategy.PrintBr(),
+	"-b": printCurrentBrStrategy.PrintBr(),
 	"--help": gtaskHelp.GtaskHelper(),
 	"-c":createTaskStrategy.CreateTask()
 }
